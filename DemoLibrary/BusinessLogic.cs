2f62411161d89efd24b138261a816530d03ac869
AlvaroAvilaPerez <Alvaro.Avila.Perez@Outlook.com>
2019-02-05 23:52:47 -0400

    Uupdated code practice Dependency Injection with Autofac

---
+++ DemoLibrary/BusinessLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DemoLibrary.Utilities;
+
+namespace DemoLibrary
+{
+    public class BusinessLogic : IBusinessLogic
+    {
+        private ILogger _logger;
+        private IDataAccess _dataAccess;
+
+        public BusinessLogic(ILogger logger, IDataAccess dataAccess)
+        {
+            _logger = logger;
+            _dataAccess = dataAccess;
+        }
+
+        public void ProcessData()
+        {
+            _logger.Log("Staring the processing of data.");
+            Console.WriteLine("Processing the data");
+            _dataAccess.LoadData();
+            _dataAccess.SaveData("ProcessedInfo");
+            _logger.Log("Finished processing of the data.");
+        }
+    }
+}
---
+++ DependencyInjectionDemo/Application.cs
@@ -0,0 +1,19 @@
+﻿using DemoLibrary;
+
+namespace DependencyInjectionDemo
+{
+    public class Application : IApplication
+    {
+        private IBusinessLogic _businessLogic;
+
+        public Application(IBusinessLogic businessLogic)
+        {
+            _businessLogic = businessLogic;
+        }
+
+        public void Run()
+        {
+            _businessLogic.ProcessData();
+        }
+    }
+}
